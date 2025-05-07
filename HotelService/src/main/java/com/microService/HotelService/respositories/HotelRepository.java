aa74c17c3195f3e98c830b78ca51d064f08d9df6
Jay Patel <jaypatel54335@gmail.com>
2025-05-07 22:42:37 +0530

    solve git problem

--- HotelService/src/main/java/com/microService/HotelService/respositories/HotelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.microService.HotelService.respositories;
-
-import com.microService.HotelService.entities.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, Integer> {
-}
